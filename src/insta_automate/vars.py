3cf600df268ace4224249946793a951f5d9f7d23
SeshuTarapatla <seshu.tarapatla@gmail.com>
2026-04-26 08:04:38 +0530

    feat: consolidate env variables and paths in vars.py
    - Added new env-based variables: `ANDROID_PIN`, `ANDROID_SERIAL`, `GIT_URL`, and `OLLAMA_URL`.
    - Reorganized code to separate env-based and derived variables for better readability and maintainability.
    - Renamed duplicate variable declarations to avoid redundancy.
    - Added new directories: `PRIVATE_DIR` and `PUBLIC_DIR`.
    - Updated `.gitignore` to exclude the `client` directory.

--- src/insta_automate/vars.py
+++ src/insta_automate/vars.py
@@ -11,26 +11,30 @@ load_dotenv()
 ASSETS = cast(Path, files("insta_automate.assets"))
 BANNER: str = (ASSETS / "banner.txt").read_text(encoding="utf-8")
 
+# Env based
+ANDROID_PIN: str = getenv("ANDROID_PIN", "")
+ANDROID_SERIAL: str = getenv("ANDROID_SERIAL", "")
+GIT_URL: str = getenv("GIT_URL", "")
 IA_ALT_ACCOUNT: str = getenv("IA_ALT_ACCOUNT", "")
+IA_DIR: Path = Path(getenv("IA_DIR", ""))
+IA_MAIN_ACCOUNT: str = getenv("IA_MAIN_ACCOUNT", "")
+OLLAMA_URL: str | None = getenv("OLLAMA_URL")
+WINDOWS_HOST: str = get_wsl_host_ip()
+
+# Derived
+ADB_SERVER_SOCKET: str = f"tcp:{WINDOWS_HOST}:5037"
+ELEMENT_HEIGHT: int = 198
+GENDER_INVALID_DIR: Path = IA_DIR / "gender_invalid"
+GENDER_VALID_DIR: Path = IA_DIR / "gender_valid"
 IA_BACKUP_CHANNEL: str = "Insta Backup"
 IA_DATABASE: str = "insta_automate"
 IA_DOCKER_IMAGE: str = "insta-automate"
 IA_ENTITY_CHANNEL: str = "Insta Automate"
 IA_FLOWS_BASE: str = "src/insta_automate/flows"
-IA_MAIN_ACCOUNT: str = getenv("IA_MAIN_ACCOUNT", "")
 IA_NOTIFY_CHANNEL: str = "Insta Notify"
 IA_PACKAGE_NAME: str = "com.instagram.android"
 IA_PREFECT_WORKPOOL: str = "insta-automate-pool"
-
-WINDOWS_HOST: str = get_wsl_host_ip()
-ADB_SERVER_SOCKET: str = f"tcp:{WINDOWS_HOST}:5037"
-ANDROID_PIN: str = getenv("ANDROID_PIN", "")
-ANDROID_SERIAL: str = getenv("ANDROID_SERIAL", "")
-ELEMENT_HEIGHT: int = 198
-GIT_URL: str = getenv("GIT_URL", "")
-IA_DIR: Path = Path(getenv("IA_DIR", ""))
-OLLAMA_URL: str | None = getenv("OLLAMA_URL")
 OLLAMA_VL_MODEL: str = "qwen3-vl:4b-instruct"
+PRIVATE_DIR: Path = IA_DIR / "private"
+PUBLIC_DIR: Path = IA_DIR / "public"
 SCANNED_DIR: Path = IA_DIR / "scanned"
-GENDER_VALID_DIR: Path = IA_DIR / "gender_valid"
-GENDER_INVALID_DIR: Path = IA_DIR / "gender_invalid"
